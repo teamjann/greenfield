9e58a502f7f2caa4d18a25d6213cff48955050a8
ntf <ntfredman@gmail.com>
2018-04-10 17:14:13 -0500

    database routes in progress

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://me:me@ds241019.mlab.com:41019/greenfield-dev');
+// mongoose.connect('mongodb://me:me@ds241019.mlab.com:41019/greenfield-dev');
+mongoose.connect('mongodb://localhost/greenfield');
+
 
 var db = mongoose.connection;
 
@@ -43,15 +45,24 @@ var Course = mongoose.model('Course', courseSchema);
 var Category = mongoose.model('Category', categorySchema);
 var User = mongoose.model('User', userSchema);
 
+var newCourse = new Course({name: "test", upvotes: 0, description: {instructor: 'Bill', price: 20}});
 
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
-  });
+
+
+var gatherCategoryNames = function(callback) {
+  Category.find()
+  .map( category => category.name )
+  .then()
+  .catch();
+
+
+  // Category.select({}, function(err, items) {
+  //   if(err) {
+  //     callback(err, null);
+  //   } else {
+  //     callback(null, items);
+  //   }
+  // });
 };
 
-module.exports.selectAll = selectAll;
+// module.exports.selectAll = selectAll;
