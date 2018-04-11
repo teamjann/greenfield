54807e7d5c07cf681b2871d820fe668c3e588238
ntf <ntfredman@gmail.com>
2018-04-11 11:18:34 -0500

    first post route setup

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,68 +1,75 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // mongoose.connect('mongodb://me:me@ds241019.mlab.com:41019/greenfield-dev');
 mongoose.connect('mongodb://localhost/greenfield');
 
+const db = mongoose.connection;
 
-var db = mongoose.connection;
-
-db.on('error', function() {
+db.on('error', () => {
   console.log('mongoose connection error');
 });
 
-db.once('open', function() {
+db.once('open', () => {
   console.log('mongoose connected successfully');
 });
 
-var courseSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
+const courseSchema = mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   name: String,
   upvotes: Number,
   description: {
     createdOn: Date,
     instructor: String,
     price: Number,
-    videoUrl: String
+    videoUrl: String,
   },
-  courseUrl: String
+  courseUrl: String,
 });
 
-var categorySchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
+const categorySchema = mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   name: String,
-  courses: [courseSchema]
+  courses: [courseSchema],
 });
 
-var userSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
+const userSchema = mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
   email: String,
   password: String,
-  coursesUpvoted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
+  coursesUpvoted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }],
 });
 
+const Course = mongoose.model('Course', courseSchema);
+const Category = mongoose.model('Category', categorySchema);
+const User = mongoose.model('User', userSchema);
 
-
-var Course = mongoose.model('Course', courseSchema);
-var Category = mongoose.model('Category', categorySchema);
-var User = mongoose.model('User', userSchema);
-
-var newCourse = new Course({name: "test", upvotes: 0, description: {instructor: 'Bill', price: 20}});
-
-
-
-var gatherCategoryNames = function(callback) {
-  Category.find()
-  .map( category => category.name )
-  .then()
-  .catch();
-
-
-  // Category.select({}, function(err, items) {
-  //   if(err) {
-  //     callback(err, null);
-  //   } else {
-  //     callback(null, items);
-  //   }
-  // });
+const insertNewCourse = function (newCourse) {
+  new Course(newCourse)
+    .save()
+    .then(() => console.log('New course successfully added!'))
+    .catch(err => console.log(err));
 };
 
+// Uncomment me later!
+// const gatherCategoryNames = function (callback) {
+//   Category.find()
+//     .map(category => category.name)
+//     .then()
+//     .catch();
+
+// const newCourse = new Course({
+//   name: 'test',
+//   upvotes: 0,
+//   description: { instructor: 'Bill', price: 20 },
+// });
+
+// Category.select({}, function(err, items) {
+//   if(err) {
+//     callback(err, null);
+//   } else {
+//     callback(null, items);
+//   }
+// });
+// };
+
 // module.exports.selectAll = selectAll;
+module.exports.insertNewCourse = insertNewCourse;
--- server/index.js
+++ server/index.js
@@ -1,26 +1,38 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../database-mongo');
 
-var items = require('../database-mongo');
-
-var app = express();
+const app = express();
 
 // UNCOMMENT FOR REACT
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(`${__dirname}/../react-client/dist`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.post('/api/categories/id/courses', (req, res) => {
+  const courseToInsert = req.body.courseToInsert;
+  console.log(courseToInsert);
 
-
-app.get('/items', function (req, res) {
-  items.selectAll(function(err, data) {
-    if(err) {
-      res.sendStatus(500);
-    } else {
-      res.json(data);
-    }
-  });
+  new Promise((resolve, reject) => {
+    resolve(db.insertNewCourse(courseToInsert));
+  })
+    .then(() => res.status(201).end())
+    .catch((err) => {
+      console.log(err);
+      res.status(500).end();
+    });
 });
 
-app.listen(3000, function() {
+// app.get('/items', (req, res) => {
+//   items.selectAll((err, data) => {
+//     if (err) {
+//       res.sendStatus(500);
+//     } else {
+//       res.json(data);
+//     }
+//   });
+// });
+
+app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
-
