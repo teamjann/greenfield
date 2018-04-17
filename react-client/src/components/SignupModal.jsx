8728db8d195082581cb5bbee2cd493c877aa62d0
nickfredman <ntfredman@gmail.com>
2018-04-17 08:51:58 -0500

    local signup working

--- react-client/src/components/SignupModal.jsx
+++ react-client/src/components/SignupModal.jsx
@@ -9,14 +9,34 @@ class SignupModal extends React.Component {
       secondPassword: '',
     };
     this.handleSignup = this.handleSignup.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeSecondPassword = this.handleChangeSecondPassword.bind(this);
   }
 
   handleSignup() {
     if (this.state.password === this.state.secondPassword) {
       this.props.addCurrentUser(this.state);
+      this.setState({
+        email: '',
+        password: '',
+        secondPassword: '',
+      });
     } else {
       window.alert('The passwords need to match!');
     }
+  }
+
+  handleChangeEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handleChangePassword(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  handleChangeSecondPassword(e) {
+    this.setState({ secondPassword: e.target.value });
   }
 
   render() {
@@ -25,11 +45,15 @@ class SignupModal extends React.Component {
         <button>Close</button>
         <form>
           <label>Username</label>
-          <input type="email" />
+          <input type="email" value={this.state.email} onChange={this.handleChangeEmail} />
           <label>Password</label>
-          <input type="password" />
+          <input type="password" value={this.state.password} onChange={this.handleChangePassword} />
           <label>Retype Password</label>
-          <input type="password" />
+          <input
+            type="password"
+            value={this.state.secondPassword}
+            onChange={this.handleChangeSecondPassword}
+          />
         </form>
         <button onClick={this.handleSignup}>Sign Up</button>
       </div>
