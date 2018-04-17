890481d16e6a15ee65e4ad5d8f76da84a7054105
HarryFray <harry.fray7@gmail.com>
2018-04-17 17:53:33 -0500

    Serialize and Deserialize user

--- database-mongo/config.js
+++ database-mongo/config.js
@@ -1,9 +1,21 @@
 const passport = require('passport');
-const LocalStrategy = require('passprot-local');
+const LocalStrategy = require('passprot-local').Strategy;
 const bodyParser = require('body-parser');
 const db = require('./index');
 
+module.exports = function (passprot) {
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  })
+}
+
+passport.use('local-signup', new LocalStrategy({
 
 
 
+}))
 
